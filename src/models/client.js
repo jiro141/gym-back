72dc5b0b77fb8382e9a7ad72f49c73501f827180
jiro141 <felipe.colmenaresvzla@gmail.com>
2024-09-30 18:56:13 -0400

    envio de imagen funcional

--- src/models/client.js
+++ src/models/client.js
@@ -74,61 +74,61 @@ Client.hasMany(Payment, { foreignKey: "clientId", as: "payments" });
 Payment.belongsTo(Client, { foreignKey: "clientId" });
 
 // Función para actualizar el estado de la membresía
-// function updateMembershipStatus(client) {
-//   const today = new Date();
+function updateMembershipStatus(client) {
+  const today = new Date();
 
-//   // Si la membresía es permanente
-//   if (client.membershipType === "permanente") {
-//     client.expirationDate = null;
-//     client.isActive = true;
-//     client.isExpired = false;
-//   } else {
-//     // Calcular la fecha de vencimiento según el tipo de membresía
-//     if (!client.renewalDate) {
-//       client.renewalDate = today;
-//     }
+  // Si la membresía es permanente
+  if (client.membershipType === "permanente") {
+    client.expirationDate = null;
+    client.isActive = true;
+    client.isExpired = false;
+  } else {
+    // Calcular la fecha de vencimiento según el tipo de membresía
+    if (!client.renewalDate) {
+      client.renewalDate = today;
+    }
 
-//     switch (client.membershipType) {
-//       case "semanal":
-//         client.expirationDate = new Date(
-//           client.renewalDate.getTime() + 7 * 24 * 60 * 60 * 1000
-//         ); // Añadir 7 días
-//         break;
-//       case "mensual":
-//         const newDate = new Date(client.renewalDate);
-//         client.expirationDate = new Date(
-//           newDate.setMonth(newDate.getMonth() + 1)
-//         ); // Añadir 1 mes
-//         break;
-//     }
+    switch (client.membershipType) {
+      case "semanal":
+        client.expirationDate = new Date(
+          client.renewalDate.getTime() + 7 * 24 * 60 * 60 * 1000
+        ); // Añadir 7 días
+        break;
+      case "mensual":
+        const newDate = new Date(client.renewalDate);
+        client.expirationDate = new Date(
+          newDate.setMonth(newDate.getMonth() + 1)
+        ); // Añadir 1 mes
+        break;
+    }
 
-//     // Actualizar el estado basado en la fecha de vencimiento
-//     if (client.expirationDate) {
-//       client.isExpired = client.expirationDate < today;
-//       client.isActive = !client.isExpired;
-//     }
+    // Actualizar el estado basado en la fecha de vencimiento
+    if (client.expirationDate) {
+      client.isExpired = client.expirationDate < today;
+      client.isActive = !client.isExpired;
+    }
 
-//     // Calcular el porcentaje de lealtad basado en la fecha de vencimiento
-//     if (client.expirationDate && !client.isExpired) {
-//       const timeDifference = client.expirationDate.getTime() - today.getTime();
-//       const daysDifference = timeDifference / (1000 * 3600 * 24); // Diferencia en días
-//       client.loyaltyPercentage = Math.max(
-//         0,
-//         Math.min(100, (daysDifference / 30) * 100)
-//       );
-//     } else {
-//       client.loyaltyPercentage = 0;
-//     }
-//   }
-// }
+    // Calcular el porcentaje de lealtad basado en la fecha de vencimiento
+    if (client.expirationDate && !client.isExpired) {
+      const timeDifference = client.expirationDate.getTime() - today.getTime();
+      const daysDifference = timeDifference / (1000 * 3600 * 24); // Diferencia en días
+      client.loyaltyPercentage = Math.max(
+        0,
+        Math.min(100, (daysDifference / 30) * 100)
+      );
+    } else {
+      client.loyaltyPercentage = 0;
+    }
+  }
+}
 
-// // Hooks para actualizar el estado de la membresía antes de crear y actualizar
-// Client.beforeCreate((client) => {
-//   updateMembershipStatus(client);
-// });
+// Hooks para actualizar el estado de la membresía antes de crear y actualizar
+Client.beforeCreate((client) => {
+  updateMembershipStatus(client);
+});
 
-// Client.beforeUpdate((client) => {
-//   updateMembershipStatus(client);
-// });
+Client.beforeUpdate((client) => {
+  updateMembershipStatus(client);
+});
 
 module.exports = Client;
